9804b1aa53b9fd6a01cc23eacae38d06bd1cea01
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-18 17:50:18 +0200

    Python Annotation & Async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,19 +9,11 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """Waits for a random delay between 0 and max_delay seconds n times.
-
-      Args:
-        n: The number of times to wait.
-        max_delay: The maximum delay in seconds.
-
-      Returns:
-        A list of the delays in seconds.
-      """
-    delays: list[float] = []
-    for _ in range(n):
-        delays.append(await wait_random(max_delay))
-
-    delays.sort()
-    return delays
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Returns the list of all the delays
+    """
+    list_delays: List[float] = []
+    for i in range(n):
+        list_delays.append(await wait_random(max_delay))
+    return sorted(list_delays)
