7c4cace173f8639783c7c1f92f4efaef5ef96600
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-19 16:21:33 +0200

    Python Annotation & Async

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""LRU Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class LRUCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
+
+    def __init__(self):
+        """Initialize"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.order.remove(key)
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    discard = self.order[0]
+                    del self.cache_data[discard]
+                    print("DISCARD: {}".format(discard))
+                    self.order.pop(0)
+                self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
