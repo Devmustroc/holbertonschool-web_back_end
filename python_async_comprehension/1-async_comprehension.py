a3c418fa9407196591413d8d6c04562c8756b9d7
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-19 15:07:56 +0200

    Python Annotation & Async

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> list[float]:
     """Collect 10 random numbers using an async comprehensing"""
-    return [i async for i in async_generator()]
+    random_numbers: list[float] = [i async for i in async_generator()]
+    return random_numbers
