4bef0b37ec26d4abad0fbded44a39b94d72671c6
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-19 15:59:02 +0200

    Python Annotation & Async

--- python_async_function/2-main.py
+++ python_async_function/2-main.py
@@ -1,8 +1,20 @@
-#!/usr/bin/env python3
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-measure_time = __import__('2-measure_runtime').measure_time
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Returns total_time / n
+    """
+    start_time: float = asyncio.get_event_loop().time()
+    await asyncio.run(wait_n(n, max_delay))
+    end_time: float = asyncio.get_event_loop().time()
+    total_time: float = end_time - start_time
+    return total_time / n
 
-n = 5
-max_delay = 9
+async def main():
+    n = 10
+    max_delay = 1000
+    print(await measure_time(n, max_delay))
 
-print(measure_time(n, max_delay))
+if __name__ == "__main__":
+    asyncio.run(main())
