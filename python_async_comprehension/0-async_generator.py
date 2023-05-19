06cc04460b65e02242c8433ca8dda397b2d0e1ef
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-19 14:47:11 +0200

    Python Annotation & Async

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """Yield a random number between 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
