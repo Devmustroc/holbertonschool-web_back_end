bb77e35729428a185998a420c3663e51173a7ffa
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-30 10:09:57 +0200

    Logging

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,6 +10,26 @@ The function should use a regex to replace occurrences of certain field values
 """
 import re
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list = None):
+        """ Constructor method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+
 
 def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
