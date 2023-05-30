c5e5d8a4f2cd7836224cd9146e9ddb417fff6985
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-30 12:11:58 +0200

    Logging

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,22 +1,13 @@
-#!/usr/bin/env python3
-"""function called filter_datum that returns the log message obfuscated:
-Arguments:
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character is separating all
-    fields in the log line (message)
-The function should use a regex to replace occurrences of certain field values
-"""
 import logging
-import csv
 import re
 import typing
+import os
+import mysql
+import mysql.connector
 from logging import StreamHandler
 from typing import List, Tuple
 
-PII_FIELDS: Tuple[str, str, str, str, str] =\
-    ("name", "email", "phone_number", "address", "credit_card")
+PII_FIELDS: Tuple[str, str, str, str, str] = ("name", "email", "phone_number", "address", "credit_card")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -28,23 +19,20 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: typing.List[str]) -> object:
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: typing.List[str]) -> None:
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Filters values in incoming log records using filter_datum """
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            message, self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
 
 
-def filter_datum(fields: typing.List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
     """ Returns the log message obfuscated """
     for field in fields:
-        message = re.sub(rf"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub(rf"{field}=.*?{separator}", f"{field}={redaction}{separator}", message)
     return message
 
 
@@ -60,13 +48,18 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> List[Tuple[str, str, str]]:
-    """ Returns a list of tuples """
-    list_of_tuples = []
-    with open('personal_data.csv', 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            list_of_tuples.append((row['name'], row['email'],
-                                   row['phone_number'], row['ssn'],
-                                   row['password']))
-    return list_of_tuples
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    # Create a connection to the database
+    db = mysql.connector.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=db_name
+    )
+    return db
