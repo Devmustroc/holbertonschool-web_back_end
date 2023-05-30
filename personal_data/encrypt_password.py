1d7ff73e1ba504a803d1ea766462f711026f505c
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-30 12:38:50 +0200

    Logging

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,8 +3,16 @@
 encrypt_password.py
 """
 import bcrypt
+from bcrypt import hashpw, gensalt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns a salted, hashed password, which is a byte string. """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """Returns a salted, hashed password, which is a byte string"""
+    salt = gensalt()
+    hashed = hashpw(password.encode(), salt)
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates if the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
