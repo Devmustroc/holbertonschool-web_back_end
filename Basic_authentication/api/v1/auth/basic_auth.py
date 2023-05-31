a8831861c5a7b3192624772989d7662d57de3774
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-31 11:39:13 +0200

    Basic authentication

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic authentication"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -15,3 +16,16 @@ class BasicAuth(Auth):
             return None
         base64_part = authorization_header.replace("Basic ", "")
         return base64_part.strip()
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header"""
+        if base64_authorization_header is None or not \
+                isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
