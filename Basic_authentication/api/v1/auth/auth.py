1508f183675beb0a2a5294a2371d3e5e6fa544e1
Devmustroc <elmustapha.abourar@outlook.com>
2023-05-31 10:40:17 +0200

    Basic authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,13 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
