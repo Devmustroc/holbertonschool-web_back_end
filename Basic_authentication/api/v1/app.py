ffd0278eefb9018542bb0bbb630f5e748dd8577f
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 12:28:10 +0200

    Basic

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -35,16 +35,21 @@ def unauthorized(error) -> str:
 
 @app.before_request
 def before_request():
-    """ Before request handler """
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
-        abort(401)
+        response = jsonify({"error": "Unauthorized"})
+        response.status_code = 401
+        response.headers['Content-Type'] = 'application/json'
+        return response
     if auth.current_user(request) is None:
-        abort(403)
+        response = jsonify({"error": "Forbidden"})
+        response.status_code = 403
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 if __name__ == "__main__":
