d274eacb40b13aedbb51d3ffbc24c40cf8f02b5f
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 12:15:36 +0200

    Basic

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required for the given path."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Get the authorization header from the request."""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get the current authenticated user."""
+        return None
