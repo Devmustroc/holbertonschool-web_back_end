2e2f7ad69e370dacc1b11abbc681f29a76048e4d
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 11:17:59 +0200

    Basic authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -24,23 +24,9 @@ else:
 
 
 @app.before_request
-def before_request() -> None:
-    """
-    Executes before every request to the api
-    """
+def before_request():
+    """ Runs before all requests """
     request.current_user = auth.current_user(request)
-
-    excluded_paths = ["/api/v1/status/",
-                      "/api/v1/unauthorized/",
-                      "/api/v1/forbidden/"]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -18,15 +18,19 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
+    """ GET /api/v1/users/:id or GET /api/v1/users/me
     Path parameter:
-      - User ID
+      - User ID or "me"
     Return:
-      - User object JSON represented
-      - 404 if the User ID doesn't exist
+      - User object JSON represented (authenticated user or user with the given ID)
+      - 404 if the User ID doesn't exist or user is not authenticated
     """
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
