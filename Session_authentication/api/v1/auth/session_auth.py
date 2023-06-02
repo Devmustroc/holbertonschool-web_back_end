6d3acf4e7ed735689fae23a3dad2aa294613000d
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 17:21:50 +0200

    Session authentication

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -36,3 +36,16 @@ class SessionAuth(Auth):
             user = User.get(user_id)
             return user
         return None
+
+    def destroy_session(self, request=None):
+        """Delete the user session / log out"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -31,3 +31,12 @@ def login() -> str:
     response.set_cookie(os.getenv("SESSION_NAME"), session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout """
+    if not SessionAuth().destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
