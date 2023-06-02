ce87bb90491f5c85671dde4cd0f54682d4c5d749
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 16:12:39 +0200

    Session authentication

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -18,8 +18,8 @@ def view_all_users() -> str:
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
+def get_user_by_id(user_id: str = None) -> str:
+    """ GET /api/v1/users/<user_id>
     Path parameter:
       - User ID
     Return:
@@ -28,10 +28,6 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    if user_id == "me" and request.current_user is None:
-        abort(404)
-    elif user_id == "me" and request.current_user:
-        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
