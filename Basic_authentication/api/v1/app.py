e918a81a419cdaf53ef05b899a8394743efce585
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 14:38:57 +0200

    BAsic

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,8 +21,8 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler """
+def Unauthorized(error) -> str:
+    """ Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
