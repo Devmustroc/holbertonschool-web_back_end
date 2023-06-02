4260660c38039acaf89075289c5a1b5dda6eb619
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 13:12:40 +0200

    Basic

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,34 +3,34 @@
 Route module for the API
 """
 from os import getenv
-import os
-from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
-auth_type = os.getenv("AUTH_TYPE")
-
-if auth_type == "basic_auth":
-    auth = BasicAuth()
-else:
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
+elif os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
 def before_request():
+    """ before_request handler """
     if auth is not None:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
-        ]
-        if request.path not in excluded_paths:
+        require_auth = auth.require_auth(request.path,
+                                         ['/api/v1/status/',
+                                          '/api/v1/unauthorized/',
+                                          '/api/v1/forbidden/'])
+        if require_auth is True:
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
@@ -44,13 +44,20 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(403)
+def Forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler """
+def Unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
