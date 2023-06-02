1dd6da348c49d642fc9f82f8cd58c8006a263bcf
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 16:23:40 +0200

    Session authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -25,16 +25,17 @@ else:
 
 
 @app.before_request
-def before_request():
-    """Before request"""
-    if auth.require_auth(request.path, ['/api/v1/status/',
-                                        '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+def before_request_func():
+    """ Executed before all request """
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if auth.require_auth(
+            request.path, excluded_paths):
+        if auth.authorization_header(request) is \
+                None and auth.session_cookie(request) is None:
             abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
