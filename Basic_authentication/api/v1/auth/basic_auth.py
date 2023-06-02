3410a2416dc3e46b51566515703cbc1489f5366a
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 13:30:29 +0200

    Base

--- Basic_authentication/api/v1/auth/basic_auth.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Class BasicAuth that inherits from Auth """
-
-from api.v1.auth.auth import Auth
-
-
-class BasicAuth(Auth):
-    """ Class BasicAuth that inherits from Auth """
-    pass
