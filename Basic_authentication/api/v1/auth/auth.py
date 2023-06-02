bdca078eb928774976af788e3c2de29947a30523
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 14:50:48 +0200

    Basic Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -3,8 +3,10 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """ Class of auth"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Check if authentication is required """
         return False
