28a1fa966dbd019c4c6b981d4e5c0b93804004ab
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 17:51:27 +0200

    Session authentication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -2,26 +2,42 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-from api.v1.auth.basic_auth import BasicAuth
+from flask import abort, jsonify, request
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.auth import Auth
+from models.user import User
 import os
+import uuid
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Create an instance of Auth based
-# on the value of AUTH_TYPE environment variable
-auth_type = os.getenv('AUTH_TYPE', 'basic_auth')
-if auth_type == 'session_auth':
-    auth = SessionAuth()
-else:
+auth = None
+
+auth_type = os.getenv("AUTH_TYPE")
+
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+elif auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
 
 
 @app.before_request
