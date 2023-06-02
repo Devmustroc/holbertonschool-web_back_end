802905352853ab0b69eee8b00d4b33c314e49b6e
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 11:52:08 +0200

    Basic

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Blueprint
 from api.v1.views import app_views
+
+app_views = Blueprint('app_views', __name__)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    abort(401)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
