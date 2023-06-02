129e6a825abd81a5bf4713b091acb5baeb880bef
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 15:15:38 +0200

    Basic Auth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,11 +31,16 @@ class BasicAuth(Auth):
         except base64.binascii.Error:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """Extract user email and password from the decoded Base64 authorization header value"""
-        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+        if decoded_base64_authorization_header \
+                is None or not \
+                isinstance(decoded_base64_authorization_header, str):
             return None, None
-        if ':' not in decoded_base64_authorization_header:
+        if ':' not in \
+                decoded_base64_authorization_header:
             return None, None
-        email, password = decoded_base64_authorization_header.split(':', 1)
+        email, password = \
+            decoded_base64_authorization_header.split(':', 1)
         return email, password
