b31cfb4e8bee8ee6cd944a07ce7d26a2682cb4f5
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-02 12:20:41 +0200

    Basic

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,7 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         for excluded_path in excluded_paths:
-            if path.startswith(excluded_path.rstrip("/") + "/"):
+            if path.rstrip("/") == excluded_path.rstrip("/"):
                 return False
         return True
 
