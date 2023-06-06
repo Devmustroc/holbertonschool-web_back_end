1d7af732f446565637a887fa29b682d6da52da83
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-06 11:13:12 +0200

    User authentication service

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -9,11 +9,15 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-    """Returns a salted, hashed password, which is a byte string"""
+    """
+    Returns a salted, hashed password,
+    which is a byte string.
+    """
     byte = password.encode("utf-8")
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(byte, salt)
     return hashed
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -28,7 +32,7 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError (f"User {email} already exists")
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             pass
         hashed = _hash_password(password).decode()
