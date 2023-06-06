316e5eefdecb14cf5c06aedbb9aa69c160e07688
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-06 11:03:26 +0200

    User authentication service

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,7 +4,7 @@ Auth methods
 """
 from db import DB
 from user import User
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -14,3 +14,24 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(byte, salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers and returns a new user if email isn't listed
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError (f"User {email} already exists")
+        except NoResultFound:
+            pass
+        hashed = _hash_password(password).decode()
+        user = self._db.add_user(email, hashed)
+        return user
