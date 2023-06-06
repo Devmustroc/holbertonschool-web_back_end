533f55a888dd09c2c21a9f30c9dd29f97888700d
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-06 11:50:06 +0200

    User authentication service

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,9 +2,9 @@
 from flask import Flask, request, jsonify
 from auth import Auth
 
-Auth = Auth()
-
 app = Flask(__name__)
+
+Auth = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -16,19 +16,17 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=["POST"], strict_slashes=False)
-def user() -> str:
-    """End-point to register a user"""
-    email = request.form.get("email")
-    password = request.form.get("password")
+@app.route('/users', methods=['POST'])
+def user_register():
+    email = request.form.get('email')
+    password = request.form.get('password')
 
     try:
-        AUTH.register_user(email, password)
+        user = Auth.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": "{}".format(email), "message": "user created"})
-
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
