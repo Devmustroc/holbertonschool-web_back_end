ddddc06a3440969c5c4f47d24bfceddcc0160a53
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-06 10:30:18 +0200

    User authentication service

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -39,3 +41,16 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Takes in arbitrary keyword arguments and returns the first row
+        found in the users table as filtered by the method’s input arguments
+        """
+        session = self._session
+        try:
+            query = session.query(User).filter_by(**kwargs).first()
+            if not query:
+                raise NoResultFound
+            return query
+        except InvalidRequestError:
+            raise
