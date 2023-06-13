3529129400cafceb0743cdb3894dca6bf763a056
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-13 10:33:14 +0200

    Unittests and Integration Tests

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 from unittest import TestCase, mock
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Any, Mapping, Sequence
 
 
 class TestAccessNestedMap(TestCase):
@@ -12,6 +13,19 @@ class TestAccessNestedMap(TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: Any) -> None:
         """Test access_nested_map function"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'")
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence,
+                                         expected: Any) -> None:
+        """Tests the access_nested_map function"""
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(expected, str(err.exception))
