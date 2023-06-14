8734a15dc0e0cf5709490007a1cf9c04c89d2330
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-14 14:22:30 +0200

    Unittests and Integration Tests

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Script Test client.py"""
+import unittest
+from unittest.mock import patch, PropertyMock
+from parameterized import parameterized
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from typing import Dict, Any, List, Callable, Generator
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, mock_get_json: Callable) -> None:
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
