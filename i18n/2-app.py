9c9ef84773b9919f14f1b4aa85a6b02ead9c26cf
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-06-15 12:58:44 +0100

    i18n

---
+++ i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config app class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
