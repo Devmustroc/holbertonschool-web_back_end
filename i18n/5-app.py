c1a0f96ad16c6f443d25306e50614b24deee0462
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-06-16 10:41:29 +0200

    i18n compiling translation

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -25,19 +25,19 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
-    """Get locale"""
+def get_locale() -> str:
+    """Get locale """
     languages = app.config['LANGUAGES']
-    locale = request.args.get('locale')
+    locale = request.args.get("locale")
     if locale and locale in languages:
         return locale
     return request.accept_languages.best_match(languages)
 
 
-def get_user() -> dict:
+def get_user() -> Dict:
     """Get user"""
     try:
-        user_id = int(request.args.get('login_as'))
+        user_id = int(request.args.get("login_as"))
         if user_id in users.keys():
             return users[user_id]
     except Exception:
@@ -46,7 +46,7 @@ def get_user() -> dict:
 
 @app.before_request
 def before_request():
-    """Find user if any and set as global on `flask.g`"""
+    """Before request"""
     user = get_user()
     if user:
         g.user = user
