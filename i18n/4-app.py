e2a0f74f3792adfea289ac93991d8cd8a6e80a87
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-06-16 09:44:02 +0200

    i18n compiling translation

---
+++ i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config app class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale"""
+    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
