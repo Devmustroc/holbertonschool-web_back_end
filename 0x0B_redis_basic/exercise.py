6e8339881349a372571e245a1e872fab9e1172df
devmustroc <elmustapha.abourar@outlook.com>
2023-06-26 13:41:27 +0200

    Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: bytes) -> str:
         """Method store"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
