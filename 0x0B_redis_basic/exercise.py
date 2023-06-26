bb4d056f6261784d70907edbd0c92b556de9215b
devmustroc <elmustapha.abourar@outlook.com>
2023-06-26 13:43:04 +0200

    Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,6 +5,8 @@ as a private variable named _redis and use the flushdb() method.
 Create store method that takes a data argument and returns a string.
 this method should generate a random key (e.g. using uuid),
 """
+from typing import Union
+
 import redis
 import uuid
 
@@ -16,7 +18,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: bytes) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method store"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
