8073e610bc2790b6e7c0fd9fd31c881f15f20139
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-28 00:45:34 +1200

    Redis increment value

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,19 +5,34 @@ as a private variable named _redis and use the flushdb() method.
 Create store method that takes a data argument and returns a string.
 this method should generate a random key (e.g. using uuid),
 """
+import functools
 from typing import Union, Optional, Callable
-
 import redis
 import uuid
 
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator count calls"""
+    key = method.__qualname__
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper"""
+        self._redis.incr(key)
+        return method(self, *args)
+
+    return wrapper
+
+
 class Cache:
     """Class Cache"""
+
     def __init__(self) -> None:
         """Constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method store"""
         key = str(uuid.uuid4())
@@ -37,5 +52,3 @@ class Cache:
     def get_int(self, key: str) -> int:
         """Method get_int"""
         return self.get(key, int)
-
-
