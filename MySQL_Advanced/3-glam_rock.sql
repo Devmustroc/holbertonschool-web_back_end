da3c40b849acfb4fbc184ebec73bb0a936998612
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-30 23:40:09 +1200

    Mysql Advanced

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Scripts that lists Glam rock as their main style,
 -- ranked by their longevity
-SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
