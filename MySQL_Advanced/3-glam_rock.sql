d1758425cce54cc823802eec96681aa9f7170546
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-01 02:06:43 +1200

    Mysql Advanced

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- Title: Glam Rock
+-- Description: Glam Rock bands and their lifespan
+-- Database: metal_bands
+SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
+FROM metal_bands
+WHERE main_style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
