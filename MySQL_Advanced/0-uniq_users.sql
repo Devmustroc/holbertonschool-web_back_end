47ed96949451b63d6157d3563fc9541c7e663297
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-30 22:26:57 +1200

    Mysql Advanced

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Create a table users with columns id, email and name.
+-- Make the email column unique.
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
