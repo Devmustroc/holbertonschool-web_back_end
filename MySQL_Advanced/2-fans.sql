8cdcd500af584057741429f10fc3f1d8c7136cb5
Devmustroc <elmustapha.abourar@outlook.com>
2023-06-30 23:23:18 +1200

    Mysql Advanced

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Ranks Country origins of bands by number of (non-unique) fans
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
