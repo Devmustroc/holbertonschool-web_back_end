da0e472daa05cd477aa7b0968b98d461ccdf21a6
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-01 02:11:06 +1200

    Mysql Advanced

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- Title: Glam Rock
 -- Description: Glam Rock bands and their lifespan
 -- Database: metal_bands
-SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`,  2023) - `formed` AS `lifespan`
+FROM `metal_bands` WHERE FIND_IN_SET('Glam Rock', `style`)
+ORDER BY `lifespan` DESC;
+
