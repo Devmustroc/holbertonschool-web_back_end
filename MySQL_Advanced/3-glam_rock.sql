3b96e69364fb0aa5cc1cdc10bff1d45d68dce911
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-01 02:05:03 +1200

    Delete file

--- MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,6 +0,0 @@
--- Scripts that lists Glam rock as their main style,
--- ranked by their longevity
-SELECT band_name, (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
