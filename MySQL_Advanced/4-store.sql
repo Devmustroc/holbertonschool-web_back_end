e2b93833694102464961e2653f4f19e7694ab8e8
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-01 01:53:43 +1200

    Mysql Advanced

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -8,7 +8,7 @@ ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
+    SET quantity = quantity - NEW.number
     WHERE item_id = NEW.item_id;
 END;//
 DELIMITER ;
