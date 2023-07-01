2d09f4f3300e899b7f690320af7c7fff539d62f6
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-02 00:25:17 +1200

    Mysql Advanced

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,6 +3,7 @@
 -- Note: An average score can be a decimal
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- * user_id, a users.id value (you can assume user_id is linked to an existing users)
+DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users SET average_score = (
