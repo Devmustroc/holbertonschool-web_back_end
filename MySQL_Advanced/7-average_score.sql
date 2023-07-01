b77aee7242e69383717b04521514498ccb8f07f6
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-02 00:21:00 +1200

    Mysql Advanced

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -4,11 +4,16 @@
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- * user_id, a users.id value (you can assume user_id is linked to an existing users)
 
-DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
+    DECLARE total_score FLOAT;
+    DECLARE project_count INT;
+    SELECT SUM(score), COUNT(*) INTO total_score, project_count
+    FROM corrections
+    WHERE user_id = user_id;
     UPDATE users
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+    SET average_score = total_score / project_count
     WHERE id = user_id;
 END //
+
 DELIMITER ;
