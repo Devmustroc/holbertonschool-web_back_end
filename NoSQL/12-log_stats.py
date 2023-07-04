aa759682ac297d9a53f6b0aeba3d1ecae3d31b74
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-04 20:19:37 +1200

    MongoDB Python

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,9 +3,29 @@
 
 from pymongo import MongoClient
 
-
-def log_stats(mongo_collection, option=None):
+if __name__ == "__main__":
     """ provides some stats about Nginx logs stored in MongoDB """
-    if option is None:
-        return mongo_collection.count_documents({})
-    return mongo_collection.count_documents({"method": option})
+    client = MongoClient('mongodb://172.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+
+    Documents = nginx_logs.count_documents({})
+    print(f"{Documents} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    check = nginx_logs.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{check} status check")
+
+    pipe = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    print("IPs:")
+    for ip in nginx_logs.aggregate(pipe):
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
