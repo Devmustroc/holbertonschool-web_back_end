b71175c89b678a63a7b4456fa37c84623ed614dd
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-04 20:41:58 +1200

    MongoDB Python

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,46 +1,20 @@
 #!/usr/bin/env python3
-"""Script that provides some stats about Nginx logs stored in MongoDB"""
-
-from pymongo import MongoClient, DESCENDING
+"""Print info in a collection"""
+from pymongo import MongoClient
 
 if __name__ == "__main__":
-
+    """ Make a check for all elements in a collention """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    ngnix_log = client.logs.nginx
+    collection = client.logs.nginx
 
-    documents = ngnix_log.count_documents({})
-    print("{} logs".format(documents))
+    print(f"{collection.estimated_document_count()} logs")
 
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("\tmethod {}: {}".format(
-            method,
-            ngnix_log.count_documents({"method": method})
-        ))
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
 
-    print("{} status check".format(
-        ngnix_log.count_documents({"$and": [{"path": "/status"},
-                                                   {"method": "GET"}]})
-    ))
-
-    pipeline = [
-            {
-                "$group": {
-                    "_id": "$ip",
-                    "count": {"$sum": 1}
-                }
-            },
-            {
-                "$sort": {
-                    "count": DESCENDING
-                }
-            },
-            {
-                "$limit": 10
-            }
-        ]
-
-    print("IPs:")
-    res = list(ngnix_log.aggregate(pipeline))
-    for item in res:
-        print("\t{}: {}".format(item.get("_id"), item.get("count")))
+    check_get = collection.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print(f"{check_get} status check")
