4dae42afacc6cddb021893e0959200d6dc383d6c
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-04 15:04:29 +0200

    ES6 Basics Arrow Function

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  }
+}
