4de30fc19f3c09f5e43aad2411568b746cc075ef
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-13 11:32:54 +0200

    ES6 Classes

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    } else {
+      throw new TypeError('Sqft must be a number');
+    }
+  }
+}
