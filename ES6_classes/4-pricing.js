43a1bcae86c1eb5331f4bbb30bf97e399a1f2d57
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-13 11:23:31 +0200

    ES6 Classes

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency === 'object') {
+      this._currency = newCurrency;
+    } else {
+      throw new TypeError('Currency must be a Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('Amount and conversionRate must be numbers');
+  }
+}
