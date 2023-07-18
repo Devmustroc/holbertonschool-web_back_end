d03a9aece37457af5cd7d70fa95f461cfb9372af
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-18 11:54:55 +0200

    ES6 data manipulation if value startswith string

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(groceries) {
+  if (groceries instanceof Map) {
+    for (const [key, value] of groceries) {
+      if (value === 1) {
+        groceries.set(key, 100);
+      }
+    }
+    return groceries;
+  }
+}
