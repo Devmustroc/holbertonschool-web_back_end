33ae54136d9c8cf61671ce5a66098fb4b03f8448
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-19 12:52:41 +0200

    run 2 file using require

--- Node_JS_basic/NodeJs/note-app/app.js
+++ Node_JS_basic/NodeJs/note-app/app.js
@@ -1,3 +1,11 @@
-const fs = require('fs');
+/*const fs = require('fs');
 fs.writeFileSync('notes.txt', 'My Name is Mustapha!');
+const msg = " " + 'i\'m a students at Holberton School';
+fs.appendFileSync('notes.txt', msg);*/
 
+// run 2 files in the same time
+require("./utils.js")
+
+const name = 'Mustapha'
+
+console.log(name)
