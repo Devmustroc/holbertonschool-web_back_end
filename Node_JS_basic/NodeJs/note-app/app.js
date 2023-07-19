810ec8e05cf90ce01d64f15471ae58cb1d476348
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-19 12:56:09 +0200

    run 2 file using require

--- Node_JS_basic/NodeJs/note-app/app.js
+++ Node_JS_basic/NodeJs/note-app/app.js
@@ -4,8 +4,6 @@ const msg = " " + 'i\'m a students at Holberton School';
 fs.appendFileSync('notes.txt', msg);*/
 
 // run 2 files in the same time
-require("./utils.js")
+const fl = require("./utils.js")
 
-const name = 'Mustapha'
-
-console.log(name)
+console.log(fl)
