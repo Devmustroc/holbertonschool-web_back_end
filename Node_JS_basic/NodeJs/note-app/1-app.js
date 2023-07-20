b03345c2d4a596dc68395dd6cb2fb1bf53dd9e6d
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-20 13:52:50 +0200

    Node JS stdin stdout

--- Node_JS_basic/NodeJs/note-app/1-app.js
+++ Node_JS_basic/NodeJs/note-app/1-app.js
@@ -1,9 +1,59 @@
 // Command line Using Yargs
-const chalk = require('chalk');
-const getNotes = require('./notes.js');
+
 const yargs = require('yargs');
-const {argv} = require("yargs");
-// costumise yargs version
-yargs.version('2.1.0')
-const arg = argv
-console.log(arg)
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Add Title',
+      demandOption: true
+    }
+  },
+  handler: () => {
+
+    console.log('Adding a new note!');
+  }
+});
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Title remove',
+      demandOption: true
+    }
+  },
+  handler: () => {
+    console.log('Removing the note!');
+  }
+})
+yargs.command({
+  command: 'list',
+  describe: 'list a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+    }
+  },
+  handler: (argv) => {
+    console.log('notes ar listed', argv)
+  }
+})
+yargs.command({
+  command: 'read',
+  describe: 'read notes',
+  builder: {
+    title: {
+      describe: 'Title read note',
+      demandOption: true
+    }
+  },
+  handler: () => {
+    console.log('reading the notes')
+  }
+})
+console.log(yargs.argv)
