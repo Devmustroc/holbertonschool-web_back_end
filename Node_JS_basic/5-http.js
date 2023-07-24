799f940fc290e09b8655937d3fa12d375d251790
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 14:16:30 +0200

    Node JS stdin stdout

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,24 +1,25 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+const database = process.argv[2];
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents('database.csv')
-      .then(() => {
-        res.end('This is the list of our students\n');
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  } else {
-    res.end('Not found');
+    res.end('This is the list of our students\n');
+    countStudents(database).then((result) => {
+      res.end(result.join('\n'));
+    }).catch((error) => {
+      res.end(`${error.message}`)
+    });
   }
 });
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+
+app.listen(port, hostname);
 
 module.exports = app;
