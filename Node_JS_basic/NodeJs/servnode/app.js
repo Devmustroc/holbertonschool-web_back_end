0317f795a9b8854877048dfdaaf53622627cf059
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 16:28:13 +0200

    Streams & Buffer

--- Node_JS_basic/NodeJs/servnode/app.js
+++ Node_JS_basic/NodeJs/servnode/app.js
@@ -1,13 +1,23 @@
 const http =  require('http');
+const fs = require('fs');
+
 
 const SerNode = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html lang="en">');
     res.write('<head><title>Inter Message</title></head>');
-    res.write('<body><form action="/message" method="post"><input type="text"><br><br><button type="submit">Submit</button></form></body>')
+    res.write('<body><form action="/message" method="POST"><input type="text"><br><br><button type="submit">Submit</button></form></body>')
     res.write('</html>');
+    return res.end()
+  }
+  if (url === '/message' && method === "POST")
+  {
+    fs.writeFileSync('newoutput.txt', 'Execute');
+    res.statusCode = 302
+    res.setHeader('Location', '/');
     return res.end()
   }
     res.setHeader('Content-Type', 'text/html');
