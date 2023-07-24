534e5c7381f27a69f54561cc3f7cd64b9a55cbfe
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 14:22:45 +0200

    Node JS stdin stdout

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = 1245;
+const app = express();
+module.exports = app;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
