8d0fe0795222d21edad1846653d1ebcfb33f8040
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 12:41:31 +0200

    Node JS stdin stdout

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,11 @@
-const http  = require('http');
+const http= require('http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 app.listen(1245, () => {
-  return;
-})
+  console.log('...');
+});
 
 module.exports = app;
