a07aa08169a2c8153178ba2be5213a718721ce44
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 10:04:06 +0200

    Node JS stdin stdout

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,45 +1,38 @@
 const fs = require('fs');
 
+
 function countStudents(path) {
   try {
-    // Read the CSV file synchronously
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = fs.readFileSync(path, {encoding: 'utf8'}).split(/\r?\n/);
 
-    // Initialize an object to store the count of students in each field
-    const studentsCountByField = {};
+    let countStudents = 0;
+    const fields = {};
 
-    // Process each line to count the students in each field
-    for (const line of lines) {
-      const [firstName, lastName, age, field] = line.split(',');
-      if (field) {
-        // Count the students in each field
-        studentsCountByField[field] = (studentsCountByField[field] || 0) + 1;
+    for (let i = 1; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line !== '') {
+        countStudents++;
+        const [fname, lname, age, field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = {
+            countField: 1,
+            students: [fname],
+          };
+        } else {
+          fields[field].countField++;
+          fields[field].students.push(fname);
+        }
       }
     }
-
-    // Calculate the total number of students
-    const totalStudents = Object.values(studentsCountByField).reduce((acc, count) => acc + count, 0);
-
-    // Log the number of students in each field and the total number of students
-    console.log(`Number of students: ${totalStudents}`);
-    for (const field in studentsCountByField) {
-      const count = studentsCountByField[field];
-      console.log(`Number of students in ${field}: ${count}. List: ${getFieldList(lines, field)}`);
+    console.log(`Number of students: ${countStudents}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].countField;
+      const names = fields[field].students.join(', ');
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
     }
-  } catch (error) {
-    // If an error occurs during reading or processing, throw the "Cannot load the database" error
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
-}
-
-function getFieldList(lines, field) {
-  // Extract the list of first names for a specific field
-  const list = lines
-    .filter(line => line.split(',')[3] === field)
-    .map(line => line.split(',')[0])
-    .join(', ');
-  return list;
 }
 
 module.exports = countStudents;
