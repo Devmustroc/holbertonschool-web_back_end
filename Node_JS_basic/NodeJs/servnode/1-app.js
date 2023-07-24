f481bb13c541392e1dcd307a0d6c1a4815d0d58b
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-24 20:33:16 +0200

    Streams & Buffer

--- Node_JS_basic/NodeJs/servnode/1-app.js
+++ Node_JS_basic/NodeJs/servnode/1-app.js
@@ -23,11 +23,14 @@ const appServer = http.createServer((req, res) => {
     req.on('end', () => {
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split('=')[1];
-      fs.writeFileSync('message.txt', `${message}`)
+      fs.writeFileSync('message.txt', `${message}`, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+
   }
   res.setHeader('Content-Type', 'text/html')
   res.write('<html lang="en">');
