dccd980c18eb958644cb2e858b296a0357c395fc
Devmustroc <elmustapha.abourar@outlook.com>
2023-07-28 17:35:12 +0200

    :zap: testing sum of two digit rounded

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,16 +1,20 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
-describe('calculateNumber', () => {
-  it('should return the sum of two rounded numbers', () => {
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+  it('Checks 1 + 3', function() {
     assert.equal(calculateNumber(1, 3), 4);
   });
-  it('should return the sum of two rounded numbers', () => {
+  it('Checks 1 + 3.7', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('should return the sum of two rounded numbers', () => {
+  it('Checks 1.2 + 3.7', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return the sum of two rounded numbers', () => {
+  it('Checks 1.5 + 3.7', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('Checks -3 + 1.2', function() {
+    assert.equal(calculateNumber(-3, 1.2), -2);
   });
 });
